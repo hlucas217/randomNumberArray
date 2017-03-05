02ae42d47ba1cecfa54b77ab76c4d43bd159b72a
Heather Lucas <hlucas@gmail.com>
2017-03-04 19:59:34 -0500

    first commit

---
+++ Array with 10 Random Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_with_10_Random_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Make an array of 10 ints and fill it with random numbers
+            // Find which one is largest (fill in any way you like)
+            // Fill it with:
+            // Random rnd = new Random();
+            // int number = rnd.Next (0, 10);
+            // this will put a random number from 0 - 9 into number
+
+            Random rnd = new Random();
+
+            int[] gameNum = new int[10];
+            int storeMax = 0;
+            
+            for (int i = 0; i <= 9; i++)
+            {
+                gameNum[i] = rnd.Next(0, 10);
+                Console.WriteLine(gameNum[i]);
+                if (gameNum[i] > storeMax)              
+                {
+                    storeMax = gameNum[i];            
+                }
+             }
+            Console.WriteLine("The maximum number is " + storeMax + ".");
+            Console.ReadLine();
+        }
+    }
+}
